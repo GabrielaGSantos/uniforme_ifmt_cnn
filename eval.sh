9fe40f1cf481acdcb1e37bcea539df180be7d17f
GabrielaGSantos <gabi.gsantos@hotmail.com>
2018-06-29 19:43:12 -0400

    modelo incluido

---
+++ eval.sh
@@ -0,0 +1,1 @@
+python3 eval.py --logtostderr --pipeline_config_path=./training/training-model.config --checkpoint_dir=./models/train --eval_dir=./models/eval
---
+++ train.sh
@@ -0,0 +1,1 @@
+python3 train.py --logtostderr --pipeline_config_path=./training/training-model.config --train_dir=./models/train
